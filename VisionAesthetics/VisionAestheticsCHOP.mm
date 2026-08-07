a70cae76da55a6d45cc0e57bc0658588926f123b
Hiroyoshi Murata <muracchijp@gmail.com>
2026-08-08 04:26:18 +0900

    VisionAesthetics / CoreLocationBeacon を main の最新状態に同期

--- VisionAesthetics/VisionAestheticsCHOP.mm
+++ VisionAesthetics/VisionAestheticsCHOP.mm
@@ -226,7 +226,7 @@ FillCHOPPluginInfo(CHOP_PluginInfo* info)
     info->customOPInfo.majorVersion = 0;
     info->customOPInfo.minorVersion = 9;
     info->customOPInfo.opIcon->setString("VAE");
-    if (info->customOPInfo.opHelpURL) info->customOPInfo.opHelpURL->setString("https://github.com/sygnalinc/TDAppleOps/blob/main/VisionAesthetics/README.md");
+    if (info->customOPInfo.opHelpURL) info->customOPInfo.opHelpURL->setString("https://github.com/sygnalinc/Apple-Frameworks-for-TouchDesigner/blob/main/VisionAesthetics/README.md");
     info->customOPInfo.minInputs = 0;
     info->customOPInfo.maxInputs = 0;
 }
